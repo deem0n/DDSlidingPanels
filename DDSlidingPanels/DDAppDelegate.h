317b1e97eba4bbec57bcec3be3bf8d1536ff1dd0
Дмитрий Дорофеев <deem0n@Dmitrij-Dorofeevs-MacBook-Pro.local>
2012-11-23 15:02:55 +0400

    Initial Commit

---
+++ DDSlidingPanels/DDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DDAppDelegate.h
+//  DDSlidingPanels
+//
+//  Created by Дмитрий Дорофеев on 11/23/12.
+//  Copyright (c) 2012 Дмитрий Дорофеев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DDSlidingPanels/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DDSlidingPanels
+//
+//  Created by Дмитрий Дорофеев on 11/23/12.
+//  Copyright (c) 2012 Дмитрий Дорофеев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DDAppDelegate class]));
+    }
+}
